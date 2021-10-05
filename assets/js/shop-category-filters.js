6cc8216ac796412d57247c348d9d303b9d933b95
WilliamDeckmann <u773560@rts-365.dk>
2021-10-05 13:22:41 +0200

    Update filters

--- assets/js/shop-category-filters.js
+++ assets/js/shop-category-filters.js
@@ -11,17 +11,45 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((result) => {
 
             // For each category filter
-            result.general.categories.forEach((item) => {
+            result.general.categoryFilters.forEach((item) => {
 
-                // Create all product elements
+                // Create container
                 let categoryItem = document.createElement("li")
                 categoryList.appendChild(categoryItem);
-                categoryItem.classList.add("Category__item");
-                categoryItem.innerHTML = `
-                    <a class="Category__link font-color_dark-brown" href="${item.link}">
-                        ${item.title}
-                    </a>
-                `;
+                categoryItem.classList.add("Category__container");
+
+                // Create button
+                let categoryButton = document.createElement("button")
+                categoryItem.appendChild(categoryButton);
+                categoryButton.classList.add("Category__button");
+
+                // Create title
+                let categoryTitle = document.createElement("p")
+                categoryButton.appendChild(categoryTitle);
+                categoryTitle.classList.add("Category__title");
+                categoryTitle.classList.add("font-color_dark-brown");
+                categoryTitle.textContent = item.title;
+
+                // Create link
+                let categoryOptions = document.createElement("ul")
+                categoryItem.appendChild(categoryOptions);
+                categoryOptions.classList.add("Category__options");
+                categoryOptions.classList.add("Category_state_inactive");
+
+                // Create link
+                item.options.forEach((option) => {
+                    let categoryLink = document.createElement("a")
+                    categoryOptions.appendChild(categoryLink);
+                    categoryLink.classList.add("Category__link");
+                    categoryLink.classList.add("font-color_dark-brown");
+                    categoryLink.textContent = option.title;
+                    categoryLink.href = option.link;
+                });
+
+                categoryButton.addEventListener("click", () => {
+                    categoryOptions.classList.toggle("Category_state_inactive")
+                    categoryTitle.classList.toggle("Category_state_active")
+                });
             });
         })
 });
